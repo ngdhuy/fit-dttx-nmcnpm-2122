0e4446143b8ce002c2fde0f092cc428f3f1ed0e0
Huy Nguyen <ndhuy@fit.hcmus.edu.vn>
2022-01-04 18:53:36 +0700

    feature(lab01): ES - Operator

---
+++ lab01/demo05.js
@@ -0,0 +1,21 @@
+/** 
+ * Các phép toán và phép so sánh
+*/
+
+let num = 100;
+console.log("num =", num);
+console.log("num = num + 1 =>", num + 1);
+console.log("num = num - 1 =>", num - 1);
+console.log("num = num * 2 =>", num * 2);
+console.log("num = num / 3 =>", num / 3);
+console.log("num = num % 3 =>", num % 3);
+console.log("num = num / 3 =>", parseInt(num / 3));
+
+console.log("num =", num);
+console.log("num++ =>", num++);
+console.log("num =", num);
+console.log("++num =>", ++num);
+console.log("num-- =>", num--);
+console.log("num =", num);
+console.log("--num =>", --num);
+
