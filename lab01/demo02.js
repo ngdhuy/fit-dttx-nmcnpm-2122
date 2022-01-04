a77112a1055aa0fa20823ce6150b10444b473f94
Huy Nguyen <ndhuy@fit.hcmus.edu.vn>
2022-01-04 18:20:19 +0700

    feature(lab01): ES - data type

---
+++ lab01/demo02.js
@@ -0,0 +1,31 @@
+/**
+ * ES - kiểu dữ liệu
+ */
+
+let num = 1;
+let float_num = 1.25;
+
+console.log("Number - Integer:", num);
+console.log("Number - Float:", float_num);
+
+let string = "Hello world";
+console.log("String:", string);
+
+let object = {
+  id: 1, 
+  name: 'Product 01', 
+  price: 1000
+};
+
+console.log("Object:", object);
+
+let array = [1, 2, 3, 4, 5];
+console.log("Array:", array);
+
+let undefine_obj; // Chưa có kiểu cố định
+console.log("undefine object:", undefine_obj);
+
+let null_obj = 100;
+console.log("null_obj:", null_obj);
+null_obj = null;
+console.log("null_obj:", null_obj);
