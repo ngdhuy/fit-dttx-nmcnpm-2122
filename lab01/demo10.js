ce7535aab8090943c084d7a9a906fa430832861a
ndhuy <ndhuy@fit.hcmus.edu.vn>
2022-01-11 19:08:04 +0700

    feature(lab01): ES - Array basic

---
+++ lab01/demo10.js
@@ -0,0 +1,35 @@
+/**
+ * Array trong ES8
+ */
+
+'use strict'
+
+console.clear()
+
+// Khai bao mang 1 chieu (array)
+let a = [1, 2, 3, 4, 5]
+
+console.log("Print array");
+for(let i = 0; i < a.length; i++) {
+  console.log(a[i])
+}
+console.log(a)
+
+console.log("Print array with arrayObject method")
+a.forEach(item => {
+  console.log(item)
+})
+
+console.log("Revert array");
+a.reverse();
+console.log(a);
+a.reverse();
+console.log(a);
+
+console.log("Insert new item to array");
+a.push(99);
+console.log(a);
+
+console.log("Remove item in array");
+a.splice(2,2);
+console.log(a);
